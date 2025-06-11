45d5feeb84f06b2bfde235b196833aab29dec160
Pedro de Castro <pedro.casstrof@gmail.com>
2025-06-11 16:26:21 -0300

    adição do to do

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesUp, faAnglesDown } from '@fortawesome/free-solid-svg-icons';
 
 function ToDoList() {
 
-    const [ tasks, setTasks ] = useState(["testando task", "testando dnv", "ultimo test"]);
+    const [tasks, setTasks] = useState(["testando task", "testando dnv", "ultimo test"]);
     const { newtask, setNewtask } = useState("");
 
     function handleInputChange(event) {
@@ -10,26 +12,30 @@ function ToDoList() {
     }
 
     function addTask() {
-        
+
     }
 
-    function deleteTask(index) {}
-    function moveTaskUp(index) {}
-    function moveTaskDown(index) {}
+    function deleteTask(index) { }
+    function moveTaskUp(index) { }
+    function moveTaskDown(index) { }
 
     return (<div className="ToDoList">
         <h1>To Do List</h1>
         <div className="inputToDoList">
             <input type="select" placeholder='Adicione uma tarefa' id="inputTask" value={newtask} onChange={handleInputChange} />
-            <button id="addbutton" onClick={addTask}>Add</button>
+            <button id="addtaskbutton" onClick={addTask}>Add</button>
         </div>
-            <ol>
-                {tasks.map((task,index) => 
-                    <li key={index}>
-                        <span className="text">{task}</span>
-                    </li>
-                )}
-            </ol>
+        <ol>
+            {tasks.map((task, index) =>
+                <li key={index}>
+                    <span className="text">{task}</span>                        <button id="removebutton" onClick={deleteTask}>REMOVE</button>
+                    <button id="movetaskup" onClick={moveTaskUp}>
+                      <FontAwesomeIcon icon={faAnglesUp} />
+                     </button>   
+                    <button id="movetaskdown" onClick={moveTaskDown}><FontAwesomeIcon icon={faAnglesDown} /></button>
+                </li>
+            )}
+        </ol>
     </div>);
 }
 
